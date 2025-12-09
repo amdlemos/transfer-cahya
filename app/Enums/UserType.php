e0455d3b1d5b0ca830e4e44f572d75e06670b9e5
Alan Lemos <amdlemos@gmail.com>
2025-12-09 10:24:39 -0300

    chore: add comentários e novo método para verificar se pode realizar transferência

--- app/Enums/UserType.php
+++ app/Enums/UserType.php
@@ -10,14 +10,35 @@ namespace App\Enums;
  */
 enum UserType: string
 {
+    /**
+     * Usuário comum do sistema
+     */
     case Common = 'common';
+
+    /**
+     * Usuário lojista com restrições de transferência
+     */
     case Merchant = 'merchant';
 
+    /**
+     * Verifica se o tipo de usuário é um lojista
+     */
     public function isMerchant(): bool
     {
         return $this === self::Merchant;
     }
 
+    /**
+     * Verifica se o usuário pode realizar transferências
+     */
+    public function canTransfer(): bool
+    {
+        return $this === self::Common;
+    }
+
+    /**
+     * Retorna o rótulo legível do tipo de usuário
+     */
     public function labels(): string
     {
         return match ($this) {
